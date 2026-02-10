bf9abec0bdba7af17a2a520b7b72de4f66a5762d
Valéria Durães <valleriaduraes@gmail.com>
2026-02-10 14:26:28 -0300

    Initializing the project, create DDL Digitalbank client domain

---
+++ README.md
@@ -0,0 +1,7 @@
+
+This is a project that will be the Integrator Project of my University Univesp.
+
+Here we are going to create a system, which is very similar to Digital Bank's system.
+
+Throught the evolviment of the project, I'll update this file and the GitHub repository, with the new features.
+
---
+++ database/ddl_digitalbank_client_domain.sql
@@ -0,0 +1,69 @@
+
+-- Projeto Integrador - Digital Bank
+-- Script DDL - Domínio de Clientes
+-- Autora: Valéria Durães Cruz
+-- Data: 2026-02
+
+
+-- DDL Data Definition Language
+
+-- 1º Create the Database
+create database digitalbank;
+
+-- 2º Create the tables
+create type doc_type_options as enum ('cpf', 'cnpj', 'passport', 'cnh', 'rg');
+
+create table client(
+	id serial primary key,
+	name varchar(255) not null,
+	doc_type doc_type_options default 'cpf'::doc_type_options not null,
+	doc_number varchar(100) unique not null,
+	birth_date date not null,
+	email varchar(255) unique not null,
+	created_at timestamp with time zone not null default now(),
+	updated_at timestamp with time zone not null default now(),
+	deleted_at timestamp with time zone null
+);
+
+create table client_phone(
+	id serial primary key,
+	phone varchar(30) not null,
+	country_code varchar(10) not null,
+	created_at timestamp with time zone not null default now(),
+	updated_at timestamp with time zone not null default now(),
+	deleted_at timestamp with time zone null,
+	client_id integer not null,
+	constraint fk_client_client_phone foreign key (client_id) references client(id)
+);
+
+create table address(
+	id serial primary key,
+	street varchar(255) not null,
+	city varchar(100) not null,
+	state varchar(30) not null,
+	country varchar(100) not null,
+	cep varchar(20) not null check(CHAR_LENGTH(cep) >= 8)
+);
+
+create table client_address(
+	id serial primary key,
+	type varchar(100) not null,
+	number varchar(20) not null,
+	complement varchar(255),
+	created_at timestamp with time zone not null default now(),
+	updated_at timestamp with time zone not null default now(),
+	deleted_at timestamp with time zone null,
+	client_id integer not null,
+	address_id integer not null,
+	constraint fk_client_address_client foreign key (client_id) references client(id),
+	constraint fk_client_address_address foreign key (address_id) references address(id)
+);
+
+
+
+
+
+
+
+
+
